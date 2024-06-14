f446b8ee0a583faf85fece1f4ef3ebb8cbf9de2c
Md kaiful Haque <kaif49490@gmail.com>
2024-06-14 14:30:59 +0530

    Learning ScrollTrigger

---
+++ Part02-Use_ScrollTrigger/script.js
@@ -0,0 +1,22 @@
+gsap.from("#page1 #box",{
+    scale:0,
+    duration:2,
+    delay:1,
+    rotate:360,
+})
+
+gsap.from("#page2 #box",{
+    scale:0,
+    duration:2,
+    delay:1,
+    rotate:360,
+    scrollTrigger:{
+        trigger:"#page2 #box",
+        scroller:"body",
+        markers:true,
+        start:'top 60%',
+        end: "top 20%",
+        scrub:true,
+    }
+})
+
